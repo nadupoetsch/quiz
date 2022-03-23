7e215a9808d219e18ca7d4ccd663895d58d949a4
Bernardo Poetsch <89049723+nadupoetsch@users.noreply.github.com>
2022-03-23 03:00:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+var a = 10, b = 10;
+
+while (a != 0){
+
+    a = a – 1;
+
+    b = b + 1;
+
+}
+*/
+
+let a = 10
+let b = 10
+while(a != 0){
+
+    a = a - 1;
+    b = b + 1;
+
+  alert('Valor do B: ' + b)
+  
+}
